08202cff6131d7b305bebeddd3db7752348beab3
lionel.burgbach <lionel.burgbacher@heig-vd.ch>
2019-05-10 18:43:36 +0200

    Extract class, field size Product to class Size and already Encapsule, Primitive obsession

--- src/main/java/heig_vd/labo4/Product.java
+++ src/main/java/heig_vd/labo4/Product.java
@@ -4,11 +4,11 @@ public class Product {
     public static final int SIZE_NOT_APPLICABLE = -1;
     private String code;
     private Color color;
-    private int size;
+    private Size size;
     private double price;
     private String currency;
 
-    public Product(String code, Color color, int size, double price, String currency) {
+    public Product(String code, Color color, Size size, double price, String currency) {
         this.code = code;
         this.color = color;
         this.size = size;
@@ -25,7 +25,7 @@ public class Product {
     }
 
     public int getSize() {
-        return size;
+        return size.getSize();
     }
 
     public double getPrice() {
---
+++ src/main/java/heig_vd/labo4/Size.java
@@ -0,0 +1,14 @@
+package heig_vd.labo4;
+
+public class Size {
+
+    private int size;
+
+    public Size(int size){
+        this.size = size;
+    }
+
+    public int getSize(){
+        return size;
+    }
+}
