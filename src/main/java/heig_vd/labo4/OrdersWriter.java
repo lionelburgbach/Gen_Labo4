16a78fdcf6c5d86f81f184f5cb32112ec0be04f7
lionel.burgbach <lionel.burgbacher@heig-vd.ch>
2019-05-10 17:33:38 +0200

    Inline Variable, method getContents(), useless object

--- src/main/java/heig_vd/labo4/OrdersWriter.java
+++ src/main/java/heig_vd/labo4/OrdersWriter.java
@@ -18,8 +18,7 @@ public class OrdersWriter {
             sb.append(", ");
             sb.append("\"products\": [");
             for (int j = 0; j < order.getProductsCount(); j++) {
-                Product product = order.getProduct(j);
-                productAttribute(sb, product);
+                productAttribute(sb, order.getProduct(j));
             }
 
             if (order.getProductsCount() > 0) {
