960241a8285519dd1a8ef221bfe26d32b84a9946
lionel.burgbach <lionel.burgbacher@heig-vd.ch>
2019-05-23 16:58:14 +0200

    Remove Dead Code, some usless gether From Order, Dead Code

--- src/main/java/heig_vd/labo4/Order.java
+++ src/main/java/heig_vd/labo4/Order.java
@@ -11,18 +11,6 @@ public class Order {
         this.id = id;
     }
 
-    public int getOrderId() {
-        return id;
-    }
-
-    public int getProductsCount() {
-        return products.size();
-    }
-
-    public Product getProduct(int j) {
-        return products.get(j);
-    }
-
     public void AddProduct(Product product) {
         products.add(product);
     }
