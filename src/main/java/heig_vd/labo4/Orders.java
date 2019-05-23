21c13379d70c2ba12d9f323a26627546e87c37eb
lionel.burgbach <lionel.burgbacher@heig-vd.ch>
2019-05-23 16:39:00 +0200

    Move Function, getContents() form OrdersWriter to Orders, Data class

--- src/main/java/heig_vd/labo4/Orders.java
+++ src/main/java/heig_vd/labo4/Orders.java
@@ -17,4 +17,34 @@ public class Orders {
     public Order getOrder(int i) {
         return orders.get(i);
     }
+
+    public String getContents(){
+
+        StringBuffer sb = new StringBuffer("{\"orders\": [");
+
+        for (int i = 0; i < orders.size(); i++) {
+            Order order = orders.get(i);
+            sb.append("{");
+            sb.append("\"id\": ");
+            sb.append(order.getOrderId());
+            sb.append(", ");
+            sb.append("\"products\": [");
+            for (int j = 0; j < order.getProductsCount(); j++) {
+                order.getProduct(j).attributeToString(sb);
+            }
+
+            if (order.getProductsCount() > 0) {
+                sb.delete(sb.length() - 2, sb.length());
+            }
+
+            sb.append("]");
+            sb.append("}, ");
+        }
+
+        if (orders.size() > 0) {
+            sb.delete(sb.length() - 2, sb.length());
+        }
+
+        return sb.append("]}").toString();
+    }
 }
--- src/main/java/heig_vd/labo4/OrdersWriter.java
+++ src/main/java/heig_vd/labo4/OrdersWriter.java
@@ -8,31 +8,6 @@ public class OrdersWriter {
     }
 
     public String getContents() {
-        StringBuffer sb = new StringBuffer("{\"orders\": [");
-
-        for (int i = 0; i < orders.getOrdersCount(); i++) {
-            Order order = orders.getOrder(i);
-            sb.append("{");
-            sb.append("\"id\": ");
-            sb.append(order.getOrderId());
-            sb.append(", ");
-            sb.append("\"products\": [");
-            for (int j = 0; j < order.getProductsCount(); j++) {
-                order.getProduct(j).attributeToString(sb);
-            }
-
-            if (order.getProductsCount() > 0) {
-                sb.delete(sb.length() - 2, sb.length());
-            }
-
-            sb.append("]");
-            sb.append("}, ");
-        }
-
-        if (orders.getOrdersCount() > 0) {
-            sb.delete(sb.length() - 2, sb.length());
-        }
-
-        return sb.append("]}").toString();
+       return orders.getContents();
     }
 }
